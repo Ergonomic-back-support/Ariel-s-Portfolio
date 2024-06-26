72923e5ac1abb9da7e34269ab68c2f8824915196
Middle Eastern Corporate Infant, who is voiced by Alec Baldwin <167745120+Ergonomic-back-support@users.noreply.github.com>
2024-06-26 14:00:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener('scroll', function() {
+    const imageContainer = document.querySelector('.image-container');
+    const rect = imageContainer.getBoundingClientRect();
+    const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
 
+    if (isVisible) {
+        imageContainer.style.opacity = 1;
+    } else {
+        imageContainer.style.opacity = 0;
+    }
+});
